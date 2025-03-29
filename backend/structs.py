1709682d501c976cc00bdbda240c61b5830ebd62
lizhe <lizhe@bicv.com>
2025-03-29 22:19:17 +0800

    增加了一些语言

--- backend/structs.py
+++ backend/structs.py
@@ -27,7 +27,17 @@ class ModelType(Enum):
 class LanguageType(Enum):
     CHINESE = "简体中文", "ZH"
     SPANISH = "西班牙语", "ES"
-    ENGLISH = "英文", "EN"
+    ENGLISH = "英文", "EN-US"
+    German = "德语", "DE"
+    PORTUGUESE = "葡萄牙语" , "PT-PT"
+    ITALIAN = "意大利语" , "IT"
+    FRENCH = "法语" , "FR"
+    RUSSIAN = "俄语" , "RU"
+    JAPANESE = "日语" , "JA"
+    KOREAN = "韩语" , "KO"
+    GREEK = "希腊语" , "EL"
+    ARABIC = "阿拉伯语", "AR"
+    DUTCH = "荷兰语" , "NL"
 
     @staticmethod
     def from_name(value: str):
@@ -35,3 +45,9 @@ class LanguageType(Enum):
             if value.strip().upper() == item.value[0].upper():
                 return item
         raise ValueError(f"{value} can not be found in {LanguageType.__name__}")
+
+
+# if __name__ == '__main__':
+#     for key, item in LanguageType.__members__.items():
+#         name = item.value[0]
+#         print(f"<item>{name}</item>")
